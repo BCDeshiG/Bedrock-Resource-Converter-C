78b67d9a2161a6475f746571e6575479c3791ece
BCDeshiG <bcdg55@gmail.com>
2021-11-15 19:11:22 +0000

    Other paintings
    The big ones that are by themselves

--- include/split.h
+++ include/split.h
@@ -4,9 +4,12 @@
 #define HEADER_INCLUDE_SPLIT
 void splitCompass(char *arg1, char *arg2, char *atlas); // Split compass/clock textures
 void splitPaintings(char *arg1, char *arg2); // Split painting into separate textures
-// Atlas, Array of names, Length of array, Unit Size, No. channels, Output path
-void splitPaintingAux(unsigned char *img, char *arr[], const int arrLen,
+// Atlas, Names, Array Length, Unit Size, Top, Width, Height, Channels, Output path
+void paintingAux1(unsigned char *img, char *arr[], const int arrLen,
 	const int q, int qTop, int qw, int qh, int ch, char *outPath);
+// Atlas, Name of painting, Unit Size, Top, Left, Width, Height, Channels, Output path
+void paintingAux2(unsigned char *img, char *name, const int q,
+	int qTop, int qLeft, int qw, int qh, int ch, char *outPath);
 // qStat[] is Top, Left, Width, Height
 char *cropPainting(unsigned char *img, int q, int qStat[4], int ch);
 #endif
--- src/split.c
+++ src/split.c
@@ -109,32 +109,56 @@ void splitPaintings(char *arg1, char *arg2){
 		return;
 	}
 	free(innPath); // Atlas loaded
-
 	const int q = floor(h/16); // Quantum cell size
-	splitPaintingAux(kz, x16P, 7, q, 0, 1, 1, ch, outPath);
-	splitPaintingAux(kz, xWide, 5, q, 2, 2, 1, ch, outPath);
-	splitPaintingAux(kz, xTall, 2, q, 4, 1, 2, ch, outPath);
-	splitPaintingAux(kz, x32P, 6, q, 8, 2, 2, ch, outPath);
-	splitPaintingAux(kz, x64P, 3, q, 12, 4, 4, ch, outPath);
+
+	// Main paintings
+	paintingAux1(kz, x16P,  7, q, 0,  1, 1, ch, outPath);
+	paintingAux1(kz, xWide, 5, q, 2,  2, 1, ch, outPath);
+	paintingAux1(kz, xTall, 2, q, 4,  1, 2, ch, outPath);
+	paintingAux1(kz, x32P,  6, q, 8,  2, 2, ch, outPath);
+	paintingAux1(kz, x64P,  3, q, 12, 4, 4, ch, outPath);
+
+	paintingAux2(kz, "fighters.png",    q, 6, 0,  4, 2, ch, outPath);
+	paintingAux2(kz, "skeleton.png",    q, 4, 11, 4, 3, ch, outPath);
+	paintingAux2(kz, "donkey_kong.png", q, 7, 11, 4, 3, ch, outPath);
+
 	stbi_image_free(kz);
 	free(outPath);
 }
 
-void splitPaintingAux(unsigned char *img, char *arr[], const int arrLen,
-	const int q, int qTop, int qw, int qh, int ch, char *outPath
-){
+void paintingAux1(unsigned char *img, char *arr[], const int arrLen,
+	const int q, int qTop, int qw, int qh, int ch, char *outPath)
+{
 	for (int i=0; i<arrLen; i++){
 		int qStat[4] = {qTop, i, qw, qh}; // (Quantum) top left width height
-		unsigned char *outIMG = cropPainting(img, q, qStat, ch);
-		char *tempSTR = calloc(strlen(outPath)+20, sizeof(char));
+		unsigned char *outIMG = cropPainting(img, q, qStat, ch); // Store cropped image
+		char *tempSTR = calloc(strlen(outPath)+20, sizeof(char)); // Store path
 		strcpy(tempSTR, outPath);
 		strcat(tempSTR, arr[i]);
 		if (stbi_write_png(tempSTR, q*qw, q*qh, ch, outIMG, q*qw*ch) == 0){
 			fprintf(stderr, "Unable to write to file\n");
 		}
+		// Free up resources
 		free(outIMG);
 		free(tempSTR);
 	}
+}
+
+void paintingAux2(unsigned char *img, char *name, const int q,
+	int qTop, int qLeft, int qw, int qh, int ch, char *outPath)
+{
+	int qStat[] = {qTop, qLeft, qw, qh};
+	unsigned char *outIMG = cropPainting(img, q, qStat, ch); // Store cropped image
+	int newLen = strlen(outPath)+strlen(name);
+	char *tempSTR = calloc(newLen+1, sizeof(char)); // Store path of output image
+	strcpy(tempSTR, outPath);
+	strcat(tempSTR, name);
+	if (stbi_write_png(tempSTR, q*qw, q*qh, ch, outIMG, q*qw*ch) == 0){
+		fprintf(stderr, "Unable to write to file\n");
+	}
+	// Free up resources
+	free(outIMG);
+	free(tempSTR);
 }
 
 char *cropPainting(unsigned char *img, int q, int qStat[4], int ch){
